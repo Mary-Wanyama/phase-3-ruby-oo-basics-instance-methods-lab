0cce00f5aae8eec09ce313426e78476d4fca9dc3
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-19 17:02:51 +0300

    completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Dog 
+
+    def bark 
+        puts "Woof!"
+    end
+    def sit 
+        puts "The Dog is sitting"
+    end
+end
+fido = Dog.new
+fido.bark
+fido.sit
+
+snoopy = Dog.new
+snoopy.bark
+snoopy.sit
+
+
+# class Dog
+#     def bark
+#       puts "Woof!"
+#     end
+#   end
+  
+#   fido = Dog.new
+#   fido.bark 
+  
+#   snoopy = Dog.new
+#   snoopy.bark 
